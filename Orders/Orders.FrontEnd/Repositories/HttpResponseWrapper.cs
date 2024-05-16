91e4d9c0b0b99893e71721fbdc3bc1b15b67b8f9
quervin <quervin.hidalgo@grupopromerica.com>
2024-05-16 17:35:08 -0600

    Beging repositpry pattern

---
+++ Orders/Orders.FrontEnd/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+
+namespace Orders.FrontEnd.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var statusCode = HttpResponseMessage.StatusCode;
+
+            switch (statusCode)
+            {
+                case HttpStatusCode.NotFound:
+                    return "Recurso no encontrado.";
+
+                case HttpStatusCode.BadRequest:
+                    return await HttpResponseMessage.Content.ReadAsStringAsync();
+
+                case HttpStatusCode.Unauthorized:
+                    return "Tienes que estar logueado para ejecutar esta operación.";
+
+                case HttpStatusCode.Forbidden:
+                    return "No tienes permisos para hacer esta operación.";
+            }
+
+            return "Ha ocurrido un error inesperado";
+        }
+    }
+}
---
+++ Orders/Orders.FrontEnd/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Orders.FrontEnd.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
+
+        Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TActionResponce>> PostAsync<T, TActionResponce>(string url, T model);
+    }
+}
