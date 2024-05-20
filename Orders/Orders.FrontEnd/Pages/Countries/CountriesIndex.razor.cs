bec995aa6c0b2db4d2279c6671936dff4433bc3f
quervin <quervin.hidalgo@grupopromerica.com>
2024-05-20 16:52:03 -0600

    add genericList Component

---
+++ Orders/Orders.FrontEnd/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Orders.FrontEnd.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.FrontEnd.Pages.Countries
+{
+    public partial class CountriesIndex
+    {
+        [Inject] private IRepository Repositry { get; set; } = null!;
+        public List<Country>? Countries { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            var responseHttp = await Repositry.GetAsync<List<Country>>("api/v1/countries");
+            Countries = responseHttp.Response;
+        }
+    }
+}
