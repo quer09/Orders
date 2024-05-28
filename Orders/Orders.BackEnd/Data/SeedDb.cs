fa7059d3d9400ea7ea4b786888789004d3c556ff
quervin <quervin.hidalgo@grupopromerica.com>
2024-05-28 09:50:56 -0600

    Add SeedDb

---
+++ Orders/Orders.BackEnd/Data/SeedDb.cs
@@ -0,0 +1,48 @@
+﻿
+using Orders.Shared.Entities;
+
+namespace Orders.BackEnd.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckCountriesAsync();
+            await CheckCategoriesAsync();
+        }
+
+        private async Task CheckCategoriesAsync()
+        {
+            if (!_context.Categories.Any())
+            {
+                _context.Categories.Add(new Category { Name = "Tecnología" });
+                _context.Categories.Add(new Category { Name = "Mascotas" });
+                _context.Categories.Add(new Category { Name = "Hogar" });
+                _context.Categories.Add(new Category { Name = "Cosmeticos" });
+                _context.Categories.Add(new Category { Name = "Licores" });
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private async Task CheckCountriesAsync()
+        {
+            if (!_context.Countries.Any())
+            {
+                _context.Countries.Add(new Country { Name = "Colombia" });
+                _context.Countries.Add(new Country { Name = "Perú" });
+                _context.Countries.Add(new Country { Name = "Argentina" });
+                _context.Countries.Add(new Country { Name = "USA" });
+                _context.Countries.Add(new Country { Name = "Italia" });
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Orders/Orders.BackEnd/Program.cs
+++ Orders/Orders.BackEnd/Program.cs
@@ -11,10 +11,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConnection"));
+builder.Services.AddTransient<SeedDb>();
+
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory!.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
+    }
+}
 
 app.UseCors(x => x
     .AllowAnyMethod()
