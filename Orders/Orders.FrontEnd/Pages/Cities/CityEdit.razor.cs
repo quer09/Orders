e388efcaeba7747970cc2960fa857d1ba4409abd
quervin <quervin.hidalgo@grupopromerica.com>
2024-06-13 16:30:28 -0600

    Create / Edit Cities

---
+++ Orders/Orders.FrontEnd/Pages/Cities/CityEdit.razor.cs
@@ -0,0 +1,61 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.FrontEnd.Repositories;
+using Orders.FrontEnd.Shared;
+using Orders.Shared.Entities;
+using System.Net;
+
+namespace Orders.FrontEnd.Pages.Cities
+{
+    public partial class CityEdit
+    {
+        private City? city;
+        private FormWithName<City>? cityForm;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [EditorRequired, Parameter] public int CityId { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var responseHttp = await Repository.GetAsync<City>($"/api/v1/cities/{CityId}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Return();
+                }
+                var messge = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", messge, SweetAlertIcon.Error);
+                return;
+            }
+            city = responseHttp.Response;
+        }
+
+        public async Task SaveAsync()
+        {
+            var responseHttp = await Repository.PutAsync($"/api/v1/cities", city);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            Return();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Cambios guardados con éxito.");
+        }
+
+        private void Return()
+        {
+            cityForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo($"/states/details/{city!.StateId}");
+        }
+    }
+}
---
+++ Orders/Orders.FrontEnd/Pages/States/StateDetails.razor.cs
@@ -0,0 +1,81 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.FrontEnd.Repositories;
+using Orders.Shared.Entities;
+using System.Net;
+
+namespace Orders.FrontEnd.Pages.States
+{
+    public partial class StateDetails
+    {
+        private State? state;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Parameter] public int StateId { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadAsync();
+        }
+
+        private async Task LoadAsync()
+        {
+            var responseHttp = await Repository.GetAsync<State>($"/api/v1/states/{StateId}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    NavigationManager.NavigateTo("/countries");
+                    return;
+                }
+
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            state = responseHttp.Response;
+        }
+
+        private async Task DeleteAsync(City city)
+        {
+            var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmación",
+                Text = $"¿Realmente deseas eliminar la cuidad? {city.Name}",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true,
+                CancelButtonText = "No",
+                ConfirmButtonText = "Si"
+            });
+
+            var confirm = string.IsNullOrEmpty(result.Value);
+            if (confirm)
+            {
+                return;
+            }
+
+            var responseHttp = await Repository.DeleteAsync<City>($"/api/v1/cities/{city.Id}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode != HttpStatusCode.NotFound)
+                {
+                    var message = await responseHttp.GetErrorMessageAsync();
+                    await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                    return;
+                }
+            }
+
+            await LoadAsync();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowCancelButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro borrado con éxito");
+        }
+    }
+}
