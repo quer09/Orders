52ec18c1807ba8f71969b34f7bb76944b47b3997
quervin <quervin.hidalgo@grupopromerica.com>
2025-01-21 12:25:35 -0600

    Order Index

--- Orders/Orders.BackEnd/Controllers/OrdersController.cs
+++ Orders/Orders.BackEnd/Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Orders.BackEnd.Helpers;
+using Orders.BackEnd.UnitsOfWork.Interfaces;
 using Orders.Shared.DTOs;
 
 namespace Orders.BackEnd.Controllers
@@ -9,13 +10,37 @@ namespace Orders.BackEnd.Controllers
     [ApiController]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/v1/[controller]")]
-    public class OrdersController: ControllerBase
+    public class OrdersController : ControllerBase
     {
         private readonly IOrdersHelper _ordersHelper;
+        private readonly IOrdersUnitOfWork _ordersUnitOfWork;
 
-        public OrdersController(IOrdersHelper ordersHelper)
+        public OrdersController(IOrdersHelper ordersHelper, IOrdersUnitOfWork ordersUnitOfWork)
         {
             _ordersHelper = ordersHelper;
+            _ordersUnitOfWork = ordersUnitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsyn([FromQuery] PaginationDTO pagination)
+        {
+            var response = await _ordersUnitOfWork.GetAsync(User.Identity!.Name!, pagination);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("totalPages")]
+        public async Task<IActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+        {
+            var response = await _ordersUnitOfWork.GetTotalPagesAsync(User.Identity!.Name!, pagination);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest();
         }
 
         [HttpPost]
---
+++ Orders/Orders.FrontEnd/Pages/Cart/OrdersIndex.razor.cs
@@ -0,0 +1,91 @@
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Orders.FrontEnd.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.FrontEnd.Pages.Cart
+{
+    [Authorize(Roles = "Admin")]
+    public partial class OrdersIndex
+    {
+        private int currentPage = 1;
+        private int totalPages;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+
+        [Parameter, SupplyParameterFromQuery] public int RecordsNumber { get; set; } = 10;
+        [Parameter, SupplyParameterFromQuery] public string Page { get; set; } = string.Empty;
+        public List<Order>? Orders { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadAsync();
+        }
+
+        private async Task SelectRecordsNumberAsync(int recordsNumber)
+        {
+            RecordsNumber = recordsNumber;
+            int page = 1;
+            await LoadAsync(page);
+            await SelectedPageAsync(page);
+        }
+
+        private async Task SelectedPageAsync(int page)
+        {
+            currentPage = page;
+            await LoadAsync(page);
+        }
+
+        private async Task LoadAsync(int page = 1)
+        {
+            if (!string.IsNullOrWhiteSpace(Page))
+            {
+                page = Convert.ToInt32(Page);
+            }
+
+            var ok = await LoadListAsync(page);
+            if (ok)
+            {
+                await LoadPagesAsync();
+            }
+        }
+
+        private void ValidateRecordsNumber(int recordsNumber)
+        {
+            if (recordsNumber == 0)
+            {
+                RecordsNumber = 10;
+            }
+        }
+
+        private async Task<bool> LoadListAsync(int page)
+        {
+            ValidateRecordsNumber(RecordsNumber);
+            var url = $"api/v1/orders?page={page}&recordsnumber={RecordsNumber}";
+            var responseHttp = await Repository.GetAsync<List<Order>>(url);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return false;
+            }
+            Orders = responseHttp.Response;
+            return true;
+        }
+
+        private async Task LoadPagesAsync()
+        {
+            ValidateRecordsNumber(RecordsNumber);
+            var url = $"api/v1/totalPages?recordsnumber={RecordsNumber}";
+            var responseHttp = await Repository.GetAsync<int>(url);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            totalPages = responseHttp.Response;
+        }
+    }
+}
